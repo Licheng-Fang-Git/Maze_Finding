f103073bcd30a12b7835dc350803509debbc6a85
Licheng Fang <andyfang1967@gmail.com>
2025-03-13 18:46:39 -0400

    Part 2: Used a Deque and the same algorithm for part 1 to find the forks and ends.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -19,17 +17,19 @@ public class Main {
             System.out.println();
         }
 
-        ArrayList<int[]> visited = new ArrayList<>();
+        parTwo(filedata);
+
+    }
+
+    public static void partOne(String[][] filedata){
+        // Part one
         Maze m = new Maze(filedata);
-
+        ArrayList<int[]> visited = new ArrayList<>();
+        int row = filedata.length;
+        int col = filedata[0].length;
         int[] arrPoints = new int[2];
-        int[] temp = new int[2];
-        arrPoints[0] = 0;
-        arrPoints[1] = 0;
-        int xPosition = 0;
-        int yPosition = 0;
-        visited.add(arrPoints);
-
+        int xPosition = arrPoints[0];
+        int yPosition = arrPoints[1];
         while (arrPoints[0] < row - 1 || arrPoints[1] < col - 1) {
             xPosition = arrPoints[0];
             yPosition = arrPoints[1];
@@ -39,28 +39,66 @@ public class Main {
                 arrPoints = m.moveTop(xPosition, yPosition);
                 visited.add(arrPoints);
             }
-
             //right
             if (m.canMoveRight(xPosition, yPosition) && !(m.containsArray(visited, m.moveRight(xPosition, yPosition)))) {
                 arrPoints = m.moveRight(xPosition, yPosition);
                 visited.add(arrPoints);
             }
-
             // left
             if (m.canMoveLeft(xPosition, yPosition) && !(m.containsArray(visited, m.moveLeft(xPosition, yPosition)))) {
                 arrPoints = m.moveLeft(xPosition, yPosition);
                 visited.add(arrPoints);
-
             }
-
             //bottom
             if (m.canMoveBottom(xPosition, yPosition) && !(m.containsArray(visited, m.moveBottom(xPosition, yPosition)))) {
                 arrPoints = m.moveBottom(xPosition, yPosition);
                 visited.add(arrPoints);
             }
-
         }
         System.out.println("(" + (row - 1) + "," + (col - 1) + ")");
+    }
+
+    public static void parTwo(String[][] filedata){
+        ArrayList<int[]> visited = new ArrayList<>();
+        Maze m = new Maze(filedata);
+
+        int[] arrPoints = new int[2];
+        arrPoints[0] = 0;
+        arrPoints[1] = 0;
+        int xPosition = 0;
+        int yPosition = 0;
+        visited.add(arrPoints);
+
+        // part Two:
+        Deque<int[]> availabePoints = new ArrayDeque<int[]>();
+        availabePoints.add(arrPoints);
+        while (!availabePoints.isEmpty()) {
+            arrPoints = availabePoints.removeFirst();
+            xPosition = arrPoints[0];
+            yPosition = arrPoints[1];
+            System.out.print("(" + xPosition + "," + yPosition + ")" + "-->");
+            // top
+            if (m.canMoveTop(xPosition, yPosition) && !(m.containsArray(visited, m.moveTop(xPosition, yPosition)))) {
+                availabePoints.add(m.moveTop(xPosition, yPosition));
+                visited.add(arrPoints);
+            }
+            //right
+            if (m.canMoveRight(xPosition, yPosition) && !(m.containsArray(visited, m.moveRight(xPosition, yPosition)))) {
+                availabePoints.add(m.moveRight(xPosition, yPosition));
+                visited.add(arrPoints);
+            }
+            // left
+            if (m.canMoveLeft(xPosition, yPosition) && !(m.containsArray(visited, m.moveLeft(xPosition, yPosition)))) {
+                availabePoints.add(m.moveLeft(xPosition, yPosition));
+                visited.add(arrPoints);
+            }
+            //bottom
+            if (m.canMoveBottom(xPosition, yPosition) && !(m.containsArray(visited, m.moveBottom(xPosition, yPosition)))) {
+                availabePoints.add( m.moveBottom(xPosition, yPosition));
+                visited.add(arrPoints);
+            }
+
+        }
     }
 
 
